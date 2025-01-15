1b96b82cabfb36816d114f25f3e18e451f216701
Alireza <alireza.alavi237@gmail.com>
2025-01-16 00:25:19 +0330

    implement an Agent class and NN file Agent class handles playing the game and learning the game using the Bellman equation the nn.py file contains a neural network class which has all the necessary functionalities to use a neural net

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import sys
-import time
+import numpy as np
 import pygame as pg
-from random import choice, randint
+from random import choice, random
 from copy import deepcopy
+from nn import NeuralNetwork
 
 # speed. the higher the fps, the faster the game
 FPS = 8
@@ -499,6 +500,105 @@ class SnakeGameGUI:
 		self.clock.tick(self.fps)
 
 		return self.game.game_over
+
+
+
+class Agent:
+	def __init__(self, train_mode: bool = False) -> None:
+		# creating the neural network
+		self.network = NeuralNetwork(
+			layers_structure=[WN*HN, 32, 32, 4],
+			activations='tanh'
+		)
+
+		self.network.weights = [
+			np.random.uniform(
+				-np.sqrt(6 / (shape[0]+shape[1])),
+				np.sqrt(6 / (shape[0]+shape[1])),
+				size=shape
+			)
+			for shape in self.network._weights_shapes
+		]
+
+		# discount factor
+		self.gamma: float = 0.9
+
+		# learning rate
+		self.alpha: float = 0.002
+
+		# epsilon-greedy policy for explore-exploit trade-off
+		# should decay over training to lower the exploration
+		if train_mode:
+			self.epsilon: float = 1
+		else:
+			self.epsilon: float = 0
+
+
+	def choose_action(self, state: list[int]) -> int:
+		"""
+		chooses and returns a move based on the current state
+		"""
+
+		actions = ['u', 'd', 'r', 'l']
+
+		# with probability epsilon, pick a random action
+		if random() < self.epsilon:
+			return choice(actions)
+
+		# otherwise pick the action with the highest Q(a, s)
+		else:
+			q_values = self.network.predict_output(state)
+
+			return max(actions, key=lambda x: q_values[x])
+
+
+	def decay_epsilon(self) -> None:
+		"""
+			decay epsilon over time to minimize exploration
+		"""
+		self.epsilon = max(0.1, self.epsilon * 0.995)
+
+
+	def update(
+			self,
+			state: list[int],
+			action: int,
+			reward: float,
+			next_state: list[int],
+			done: bool
+	) -> None:
+		"""
+			updates the neural network using the Bellman equation.
+
+			@param state: the current state of the game
+			@param action: the action that the agent picked for the state
+			@param reward: the reward that the agent received for the action
+			@param next_state: the state after the agent's action
+			@param done: if the game is over or not
+		"""
+		q_values = self.network.predict_output(state)
+		target_q_values = q_values.copy()
+
+		if done:
+			# game is over
+			target_q_values[action] = reward
+		else:
+			# using Bellman equation to compute the target
+			next_q_values = self.network.predict_output(next_state)
+			target_q_values[action] = reward + self.gamma * np.max(next_q_values)
+
+		# train the network
+		state = np.array(state).reshape((-1, 1))
+		target_q_values = np.array(target_q_values).reshape((-1, 1))
+		self.network.train(
+			x_train=state,
+			y_train=target_q_values,
+			learning_rate=self.alpha,
+			constant_lr=True,
+			batch_size=1,
+			number_of_epochs=5,
+			verbose=False
+		)
 
 
 if __name__ == '__main__':
