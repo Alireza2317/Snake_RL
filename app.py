aaaf8d6417bfda6940c149693fe234d0bf213716
Alireza Alavi <alireza.alavi237@gmail.com>
2025-01-19 03:08:41 +0330

    implemented a trainer class this class combines the game and agent efficiently and trains the agent with its useful methods

--- app.py
+++ app.py
@@ -2,112 +2,139 @@ from snake import SnakeGame, SnakeGameGUI, Direction, Reward
 from agent import Agent
 from matplotlib import pyplot as plt
 
+class Trainer:
+	def __init__(self, episodes: int, render: bool = False, **kwargs):
+		self.agent = Agent(train_mode=True, **kwargs)
+		self.game = SnakeGameGUI() if render else SnakeGame()
+		self.num_episodes = episodes
 
-def live_plot(x, y):
-	plt.ion()
-	plt.clf()
-
-	plt.xlabel('episode')
-	plt.title('progress')
-
-	plt.xlim(0, x[-1]+len(x)//5)
-	plt.plot(x, y)
-	plt.text(x[-1], y[-1], f'{y[-1]:.3f}')
-
-	plt.show(block=False)
-	plt.pause(0.0001)
+		# number of points to plot
+		self.log_freqency = episodes // (episodes if episodes < 100 else 100)
 
 
-def train_agent(resume: bool, episodes: int, render: bool = False):
-	agent = Agent(train_mode=True, init_xavier=1, activations='tanh')
+	def train_step(self) -> tuple[int, float]:
+		episode_reward: float = 0
 
-	if render:
-		game = SnakeGameGUI()
-		game.fps = 40
-	else:
-		game = SnakeGame()
-
-	total_reward: float = 0
-	rewards_average = [0]
-	steps_survived_list = [0]
-	episodes_plot = [1]
-
-
-	if resume:
-		agent.load_params()
-
-	# number of points to plot
-	NUM_POINTS = episodes if episodes < 100 else 100
-
-	for episode in range(1, episodes+1):
-		steps_survived: int = 0
-
-		state = game.get_state()
+		state = self.game.get_state()
 
 		# each loop corresponds to one whole game
-		done = False
-		while not done:
-			steps_survived += 1
-
+		while not self.game.game_over:
 			# agent chooses an action
-			ai_action: Direction = agent.choose_action(state)
-
+			ai_action: Direction = self.agent.choose_action(state)
 			# applying the action to the actual game
-			game.action = ai_action
-			next_state, reward, done = game.step()
+			self.game.action = ai_action
 
-			total_reward += reward
+			next_state, reward = self.game.step()
 
-			# convert the action from Direction to int index
-			action: int = game.action.value
+			episode_reward += reward
 
 			# call the short train on this current experience and save it to buffer
-			agent.update_short(state, action, reward, next_state, done)
+			self.agent.update_short(
+				state, ai_action.value, reward, next_state, self.game.game_over
+			)
 
-			if reward >= Reward.GROW.value or reward <= Reward.DIE.value:
-				agent.replay_buffer.add(state, action, reward, next_state, done)
+			self.agent.replay_buffer.add(
+				state, ai_action.value, reward, next_state, self.game.game_over
+			)
 
 			# transition the states
 			state = next_state
 
-		# the game is over so done=True here
-		game.reset()
+		# since the game is over
+		self.game.reset()
 
-		# updating the agent with the replay buffer
-		agent.update_with_memory()
-
-		# lower the exploration probability after each episode
-		agent.decay_epsilon()
-
-		# printing and plotting some data
-		if episode % (episodes // NUM_POINTS) == 0:
-			avg_reward: float = total_reward / episode
-			episodes_plot.append(episode)
-			steps_survived_list.append(steps_survived)
-			rewards_average.append(avg_reward)
-
-			print(
-				f'Episode {episode}:',
-				f'{total_reward=:.3f},',
-				f'avg_reward={avg_reward:.3f},',
-				f'{steps_survived=}, epsilon={agent.epsilon:.3f}',
-				sep=' '
-			)
-
-			live_plot(episodes_plot, rewards_average)
-
-		# save parameters every episode
-		agent.save_params()
+		return self.game.survival_score, episode_reward
 
 
-	# plot the whole data in the end of training
-	plt.clf()
-	plt.plot(
-		episodes_plot, rewards_average,
-		episodes_plot, steps_survived_list
-	)
-	plt.legend(['average rewards', 'steps survived'])
-	plt.show()
+	def train(self):
+		total_reward = 0
+		avg_rewards = []
+		surviveds = []
+		episodes = []
+
+		for episode in range(1, self.num_episodes+1):
+			survived, episode_reward = self.train_step()
+			total_reward += episode_reward
+
+			# updating the agent with the replay buffer
+			# also decaying epsilon
+			# and save parameters every episode
+			self.update_agent()
+
+			if (episode % self.log_freqency) == 0:
+				avg_reward = total_reward / episode
+				self.log_and_plot(
+					episode, total_reward, avg_reward, survived,
+					episodes, avg_rewards, surviveds
+				)
+
+		self.final_plot(episodes, avg_rewards, surviveds)
+
+
+	def update_agent(self):
+		self.agent.update_with_memory()
+		self.agent.decay_epsilon()
+		self.agent.save_params()
+
+
+	def log_and_plot(
+			self,
+			episode: int,
+			total_reward: float,
+			avg_reward: float,
+			survived: int,
+			episodes: list[int],
+			avg_rewards: list[float],
+			surviveds: list[int]
+	):
+		print(
+			f'Episode {episode}:',
+	  	 	f'total_reward={total_reward:.2f},',
+	 	   	f'avg_reward={avg_reward:.2f},',
+	  	  	f'steps_survived={survived}, epsilon={self.agent.epsilon:.2f}',
+	   		sep=' '
+		)
+
+		episodes.append(episode)
+		avg_rewards.append(avg_reward)
+		surviveds.append(survived)
+
+		self.live_plot(episodes, avg_rewards)
+
+
+	def live_plot(self, x, y):
+		plt.ion()
+		plt.clf()
+
+		plt.xlabel('episode')
+		plt.title('progress')
+
+		plt.xlim(0, x[-1]+len(x)//5)
+		plt.plot(x, y)
+		plt.text(x[-1], y[-1], f'{y[-1]:.3f}')
+
+		plt.pause(0.01)
+		plt.show(block=False)
+
+
+	def final_plot(
+		self,
+		episodes: list[int],
+		avg_rewards: list[float],
+		surviveds: list[int]
+	):
+		plt.show()
+		plt.ioff()
+		plt.figure(2)
+		plt.clf()
+
+		plt.plot(
+			episodes, avg_rewards,
+			episodes, surviveds
+		)
+		plt.legend(['average rewards', 'steps survived'])
+		plt.show()
+
 
 
 def play():
@@ -129,5 +156,6 @@ def play():
 
 
 if __name__ == '__main__':
-	train_agent(resume=False, episodes=1000, render=False)
-	play()
+	trainer = Trainer(episodes=2, render=False)
+
+	trainer.train()
